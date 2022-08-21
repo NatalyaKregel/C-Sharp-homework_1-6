db37f85e05dc3caf305f0f46d28d39a651609e9b
Nata Kregel <natik.kregel@gmai.com>
2022-08-21 19:43:05 +0300

    Добавлена Задача №6 по С#

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N включительно.
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int x=0;
+
+while ( x < N )
+{
+    x = x + 2;
+    
+    Console.Write("Четные числа: ");
+    Console.WriteLine(x);
+}
